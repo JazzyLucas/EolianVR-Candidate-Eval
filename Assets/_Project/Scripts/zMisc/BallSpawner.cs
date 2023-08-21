6811f230fe5eb20355c8bf28333d7f97721c3985
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2023-08-20 21:34:42 -0400

    removed GrowTransformOverTime and custom Terrain
    these were unnecessary

--- Assets/_Project/Scripts/zMisc/BallSpawner.cs
+++ Assets/_Project/Scripts/zMisc/BallSpawner.cs
@@ -14,21 +14,11 @@ public class BallSpawner : MonoBehaviour
         newBallT.localScale = Vector3.one * 0.03f;
         newBallT.position = click.clickPosition;
         newBallT.rotation = click.clickRotation;
-        StartCoroutine(GrowTransformOverTime(newBallT, 0.1f, 0.01f, 5f));
         // Color stuff
         var renderer = newBall.GetComponent<Renderer>();
-        renderer.material.color = new Color(Random.Range(0f,1f), Random.Range(0f,1f), Random.Range(0f,1f), 1f);
+        renderer.material.color = new(Random.Range(0f,1f), Random.Range(0f,1f), Random.Range(0f,1f), 1f);
         // Rigidbody stuff
         var rb = newBall.AddComponent<Rigidbody>();
         rb.AddForce(newBallT.forward * 30, ForceMode.Impulse);
-    }
-    
-    IEnumerator GrowTransformOverTime(Transform transformToGrow, float scaleAdditiveOnIntervals, float interval, float time)
-    {
-        for (float i = 0; i < time; i+= interval)
-        {
-            transformToGrow.localScale += (Vector3.one * scaleAdditiveOnIntervals);
-            yield return new WaitForSeconds(interval);
-        }
     }
 }
